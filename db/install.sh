f910c22000fd585006431068541bf33409b46d32
CandiceMiao <CandiceMiao@CandiceMiaos-MacBook-Pro.local>
2018-10-21 09:50:59 -0700

    new shell for install

--- db/install.sh
+++ db/install.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-cd ~
-wget https://nodejs.org/dist/v10.12.0/node-v10.12.0.tar.gz
-tar xvf node-v10.12.0-linux-x64.tar.xz
-mv node-v10.12.0-linux-x64 nodejs
-mkdir ~/bin
-cp nodejs/bin/node ~/bin
-cd ~/bin
-ln -s ../nodejs/lib/node_modules/npm/bin/npm-cli.js npm
-node --version
-npm --version
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
+nodejs -v
 
 cd /home/dubhacks2018/dubhacks2018/dubhacks2018/db
 npm install -save
